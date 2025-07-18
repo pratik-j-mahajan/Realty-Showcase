5f17c1510422f5040b57cc69613064c2ba4a766d
Pratik Mahajan <mahajanpratik0612@gmail.com>
2025-07-18 11:48:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+Shery.mouseFollower();
+Shery.makeMagnet(".AA");
+
+Shery.hoverWithMediaCircle(".BB", {
+    videos: [  "./3.mp4"],
+  });
+
+shery.imageffect("", {style:5 , debug: true , gooey: true});
